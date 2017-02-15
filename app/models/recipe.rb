a208be597809b886c3b86f76d8a27c94bd2c7209
Adam Friedl <ajfriedl@gmail.com>
2017-02-14 23:29:36 -0500

    Messed up root

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,11 +2,15 @@ class Recipe < ApplicationRecord
 	belongs_to :user
 	has_many :recipe_ingredients
 	has_many :ingredients, through: :recipe_ingredients
-	accepts_nested_attributes_for :ingredients, :reject_if => proc { |attributes| attributes['name'].blank? }
+
+	validates :name, presence: true
+	validates_associated :ingredients
 
 	# Scope method
   scope :most_recent_contributor, -> { order(created_at: :desc).first.user }
 
+	# This creates duplicate nested items. Cannot use in final project.
+	accepts_nested_attributes_for :ingredients, :reject_if => proc { |attributes| attributes['name'].blank? }
 
 	# def ingredients_attributes=(ingredient)
 	# 	self.ingredients = Ingredient.find_or_create_by(name: ingredient.name)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  devise_scope :user do
+  	root 'devise/sessions#new'
+  end
 
   resources :ingredients
   resources :recipes, only: [:index, :show]
